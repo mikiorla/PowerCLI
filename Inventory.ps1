cffc7c67accca625f77910581b9b534223e30890
mikiorla <mikiorla@gmail.com>
2015-09-02 13:09:46 +0200

    added new scripts

---
+++ Inventory.ps1
@@ -0,0 +1,39 @@
+﻿#http://www.wooditwork.com/2011/08/11/adding-vmx-files-to-vcenter-inventory-with-powercli-gets-even-easier/
+
+
+$powerCLIpssnapin = 'VMware.VimAutomation.Core'
+
+Add-PSSnapin $powerCLIpssnapin
+Get-PSSnapin $powerCLIpssnapin
+
+Get-Command -Module $powerCLIpssnapin
+
+Get-vm
+
+Connect-VIServer -Server vcentar.ktehnika.local
+
+$ESXHost = "esxir900.ktehnika.local"
+
+Get-VMHost $ESXHost | Get-VM
+
+Get-Datastore
+$VMDatastore = 'DELL 900 1Tb'
+
+Get-Datastore -Name $VMDatastore
+Get-Datastore -Name $VMDatastore | Get-VM
+
+$VMFilePath = '[DELL 900 1Tb] hv2/hv2.vmx'
+$ESXHost = "esxir900.ktehnika.local"
+$VMfolder = 'PrivateCloud'
+
+#import in inventory
+New-VM -VMFilePath $VMFilePath -VMHost $ESXHost -Location $VMFolder 
+
+Get-VM hv2 | fl *
+Start-VM hv2
+
+Get-VM hv2 | Remove-VM
+
+Get-VM _hv1 | Remove-VM -Confirm:$false
+New-VM -VMFilePath '[FAS2020] hv1/_hv1.vmx' -VMHost esxirx300.ktehnika.local -Location $VMFolder 
+Start-VM _hv1
---
+++ Misc.ps1
@@ -0,0 +1,6 @@
+﻿$a = gwmi -Class Win32_NetworkAdapterConfiguration 
+$a | gm
+$a | fl *
+
+
+(gwmi -Class win32_process -ComputerName test-hy1 -Filter 'Name="wininit.exe"').Terminate()
