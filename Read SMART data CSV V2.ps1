2d0fc5bab83f3484f43886b7ed2333afe6c0ddf4
Milan Orlovic <mikiorla@gmail.com>
2019-03-06 15:03:06 +0100

    some minor changes

--- Read SMART data CSV V2.ps1
+++ Read SMART data CSV V2.ps1
@@ -3,20 +3,18 @@
 #import-module VMware.VimAutomation.Core
 Import-Module VMware.PowerCLI
 
-
+$pathToCSV = " ... "
 $date = (Get-Date).DateTime.replace(" ",".").replace(",",".").replace("..",".")
-$hosts = Get-Datacenter cms2 | Get-VMHost | sort Name
-#$hosts = Get-cluster | Get-VMHost | sort Name
+$hosts = Get-cluster | Get-VMHost | sort Name
 #$hosts = Get-VMHost bvs0002.cms.zz
 $up = "↑"
 $down = "↓"
 foreach ($vmhost in $hosts)
 {
 
-$host_csv = import-csv "C:\Users\b280082_adm\Documents\$($vmhost.Name)_SMART.csv" -Delimiter "," -ErrorAction Continue
+$host_csv = import-csv "$pathToCSV\$($vmhost.Name)_SMART.csv" -Delimiter "," -ErrorAction Continue
 
 $esxcli = Get-EsxCli -VMHost $vmhost -V2
-
 $deviceList = $esxcli.storage.core.device.list.Invoke() | ? {($_.DeviceType -ne "CD-ROM") -and ($_.IsUSB -eq $false)}
 $arg = $esxcli.storage.core.device.smart.get.CreateArgs()
 
@@ -25,20 +23,15 @@ foreach($device in $deviceList)
 {
 $ReadCountersSame,$WriteCountersSame=$false
 $Error.Clear()
-#$device.DisplayName 
-
 try {
     $host_csv_device = $host_csv|?{$_.DeviceName -eq $device.DisplayName}
-    #Date,DeviceName,ReadErrorCount,WriteErrorCount,HealthStatus,InitialBadBlockCount    
     $previousReadErrorCount = ($host_csv_device.ReadErrorCount | select -Last 1).Split(" ")[0]
     $previousWriteErrorCount = ($host_csv_device.WriteErrorCount | select -Last 1).Split(" ")[0]
     $previousInitialBadBlockCount = ($host_csv_device.InitialBadBlockCount | select -Last 1).Split(" ")[0]    
     
-    #"$($device.DisplayName) Previous Write error: $previousWriteErrorCount WriteErrorCount $($host_csv_device.WriteErrorCount.Count) WriteErrorCountLastIndex $($host_csv_device.WriteErrorCount.Count-1)"
-
     $arg.devicename =$device.Device
     $smart = $esxcli.storage.core.device.smart.get.Invoke($arg)
-        
+    
     $ReadErrorCount = ($smart | ? {$_.Parameter -contains "Read Error Count"} ).value
     $WriteErrorCount = ($smart | ? {$_.Parameter -contains "Write Error Count"} ).value
     $HealthStatus = ($smart | ? {$_.Parameter -contains "Health Status"} ).value
@@ -55,7 +48,6 @@ try {
     if (!$WriteCountersSame){"$($device.DisplayName) Previous.WriteErrorCount $previousWriteErrorCount New.WriteErrorCount $($host_device.WriteErrorCount)"}
     if (!$ReadCountersSame){"$($device.DisplayName) Previous.ReadErrorCount $previousReadErrorCount New.ReadErrorCount $($host_device.ReadErrorCount)"}
     
-    #$host_csv = $host_csv | Export-Csv -Append -Path "C:\Users\b280082_adm\Documents\$($vmhost.Name)_SMART.csv" -NoTypeInformation -NoClobber
     $host_device | Export-Csv -Append -Path "C:\Users\b280082_adm\Documents\$($vmhost.Name)_SMART.csv" -NoTypeInformation -NoClobber
 
 }
@@ -65,7 +57,3 @@ catch {Write-Warning $Error.Exception.Message}
 }
 
 
-#$host_csv = $host_csv_pso | Export-Csv -Append -Path "C:\Users\b280082_adm\Documents\$($vmhost.Name)_SMART.csv" -NoTypeInformation -NoClobber
-
-
-}
